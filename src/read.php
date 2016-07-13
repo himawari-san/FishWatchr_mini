49b68eec91d6e4ebea0eb70ecc39fe97c9b15e2b
himawari-san <himawari@ninjal.ac.jp>
2016-07-13 18:22:10 +0900

    detect encoding of setting files

--- src/read.php
+++ src/read.php
@@ -5,6 +5,11 @@ header('Content-Type: application/json');
 $url = $_POST['url'];
 $json_page = file_get_contents($url);
 
+$encode = mb_detect_encoding($json_page, "UTF-8, SJIS, EUC-JP, JIS, ASCII");
+if($encode !== "UTF-8"){
+  $json_page = mb_convert_encoding($json_page, "UTF-8", $encode);
+}
+
 echo $json_page;
 
 ?>
